999f2cab9b4e7ae6794d02777e44dc1373012e2c
kees <kees@incedo.nl>
2023-04-03 23:05:22 +0200

    Adding some routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Menu from './components/Menu';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <main>
-        <Login />
-        <Register />
-      </main>
+      <BrowserRouter>
+        <Menu />
+        <header className="App-header">
+          <h1>My Example React App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/"  element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
-// src/components/Menu.js
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Menu.css';
 
 function Menu() {
@@ -13,9 +13,11 @@ function Menu() {
     <nav className="menu">
       <div className="menu-logo">My App</div>
       <div className={`menu-items ${menuOpen ? 'menu-open' : ''}`}>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <Link to="/" onClick={toggleMenu}>Home</Link>
+        <Link to="/about" onClick={toggleMenu}>About</Link>
+        <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+        <Link to="/login" onClick={toggleMenu}>Login</Link>
+        <Link to="/register" onClick={toggleMenu}>Register</Link>
       </div>
       <button className="menu-toggle" onClick={toggleMenu}>
         {menuOpen ? 'Close' : 'Menu'}
