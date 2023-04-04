bef7db8ca398c3fb1609dc2d92642d77a1a06438
kees <kees@incedo.nl>
2023-04-04 07:32:42 +0200

    extract url as environment var

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:5000';
+import axiosInstance from './axiosInstance';
 
 export async function getProfile() {
   try {
-    const response = await axios.get(`${API_BASE_URL}/profile`);
+    const response = await axiosInstance.get(`/profile`);
     return response.data;
   } catch (error) {
     console.error('Error fetching profile:', error);
